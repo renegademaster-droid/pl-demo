943ee66ade276e33ca2ac087f29ec88b0f469440
renegademaster-droid <Lissabon2017!!>
2026-02-28 12:55:06 +0200

    repo name

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// GitHub Pages: base = '/repo-name/' (vaihda jos repon nimi on eri)
+// GitHub Pages: base = repon nimi (https://user.github.io/pl-demo/)
 export default defineConfig({
-  base: '/demosite/',
+  base: '/pl-demo/',
   plugins: [react()],
 })
